7b678f99ce4aa133d2d20abe173ff955ae89e1a3
nguimkia <nguimkiamael5@gmail.com>
2026-06-24 22:53:11 +0200

    Fix API URL to production backend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE = 'http://127.0.0.1:8000/api';
+const API_BASE = 'https://agroconseil-backend-2.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE,
